647ff51a9f87683d848a11956cb4154778eb5cf4
DELL <DELL@DELL-PC>
2019-09-19 09:59:39 +0530

    First Commit
    Uploading all the files

---
+++ MavenExample/src/main/java/com/cmsedge/MavenExample/App.java
@@ -0,0 +1,13 @@
+package com.cmsedge.MavenExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
